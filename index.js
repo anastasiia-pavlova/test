364241fdf5251e16c4b5f8458fbbc6887095eca1
anastasiia-pavlova <anastasiia.pavlova@ukr.net>
2021-11-17 18:43:36 +0200

    start

---
+++ index.js
@@ -0,0 +1,8 @@
+import './sass/main.scss';
+import './js/slider.js';
+import './js/modal.js';
+import './js/modal-map';
+import './js/mobile-menu.js';
+import './js/anchor.js';
+import './js/accordion-card.js';
+// import './js/header-fixed.js';
